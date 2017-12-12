8dac7909e999b8d96611b5576491d3721881f4b9
Abdullah Ahmad <ahmada1uk@googlemail.com>
2017-12-12 11:39:19 +0000

    Pointers to pointers you dumbo

--- NW.c
+++ NW.c
@@ -12,35 +12,30 @@ typedef struct seq_info {
         char *prot_seq;
 } seq_t;
 
-void SeqRead(char *fpath, char *input_arr);
-void SeqParse(char *input_arr, seq_t *Prot);
+void SeqRead(char *fpath, char **input_arr);
+void SeqParse(char **input_arr, seq_t **Prot);
 int SeqLength(char *input_arr);
 
 int main(int argc, char **argv){
         char *seq;
         seq_t *woa;
-        SeqRead(argv[1], seq);
+        SeqRead(argv[1], &seq);
+        SeqParse(&seq, &woa);
         free(seq);
 }
 
-void SeqRead(char *fpath, char *input_arr){
+void SeqRead(char *fpath, char **input_arr){
         FILE * file;
         size_t size = 0;
         file = fopen(fpath, "r");
         fseek(file, 0, SEEK_END);
         size = ftell(file);
         rewind(file);
-        input_arr = malloc((size + 1) * sizeof(*input_arr));
-        fread(input_arr, size, 1, file);
-        input_arr[size] = '\0';
-        printf("%s", input_arr);
+        *input_arr = malloc((size + 1) * sizeof(char));
+        fread(*input_arr, size, sizeof(char), file);
+        (*input_arr)[size] = '\0';
 }
 
-void SeqParse(char *input_arr, seq_t *Prot){
-        int i, nc;
-        i = 0;
-        while (input_arr[i] != '\0'){
-               printf("%c", input_arr[i]);
-                ++i;
-        }
+void SeqParse(char **input_arr, seq_t **Prot){
+        printf("%s\n", *input_arr);
 }
