629d0c5ec8686d90ed9cba7db1c2828982ead1a8
Abdullah Ahmad <ahmada1uk@googlemail.com>
2017-12-12 16:16:42 +0000

    Working parser

--- NW.c
+++ NW.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 200
 /*
  * TODO:
  * -Sequence Parser
@@ -8,19 +7,20 @@
  */
 
 typedef struct seq_info {
-        char *prot_id;
+        char *name;
         char *prot_seq;
 } seq_t;
 
 void SeqRead(char *fpath, char **input_arr);
-void SeqParse(char **input_arr, seq_t **Prot);
+void SeqParse(char **input_arr, seq_t *Prot);
 int SeqLength(char *input_arr);
 
 int main(int argc, char **argv){
         char *seq;
-        seq_t *woa;
+        seq_t woa;
         SeqRead(argv[1], &seq);
         SeqParse(&seq, &woa);
+        printf("%s\n%d\n", woa.name, SeqLength(seq));
         free(seq);
 }
 
@@ -36,6 +36,41 @@ void SeqRead(char *fpath, char **input_arr){
         (*input_arr)[size] = '\0';
 }
 
-void SeqParse(char **input_arr, seq_t **Prot){
-        printf("%s\n", *input_arr);
+void SeqParse(char **input_arr, seq_t *Prot){
+        int i = 0;
+        int linecount = 0;
+        Prot->name = malloc((5) * sizeof(char));
+        while((*input_arr)[i] != '\0'){
+                if ((*input_arr)[i] == '>'){
+                        Prot->name[0] = (*input_arr)[i+1];
+                        Prot->name[1] = (*input_arr)[i+2];
+                        Prot->name[2] = (*input_arr)[i+3];
+                        Prot->name[3] = (*input_arr)[i+4];
+                        Prot->name[4] = '\0';
+                }
+                if ((*input_arr)[i] == '\n'){
+                        linecount++;
+                        ++i;
+                }
+                if (linecount > 0){
+                        printf("%c", (*input_arr)[i]);
+                }
+                ++i;
+        }
+        printf("\n");
+}
+
+int SeqLength(char *input_arr){
+        int i, linecount, wc;
+        i = linecount = wc = 0;
+        while(input_arr[i] != '\0'){
+                if (input_arr[i] == '\n'){
+                        linecount++;
+                        i++;
+                }
+                if (linecount > 0){
+                        printf("%c", input_arr[i]);
+                        }
+                }
+        return wc;
 }
